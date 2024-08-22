b3fa41265c7ef4efd49551b1ceac4b96485795b0
Sabatino Masala <Masalasabatino@gmail.com>
2024-08-22 22:14:53 +0200

    Add pail

--- config/dev-services.php
+++ config/dev-services.php
@@ -7,6 +7,14 @@ return [
             'style' => ['green', null, ['bold']],
             'logging' => true
         ],
+        'pail' => [
+            'command' => ['php', 'artisan', 'pail'],
+            'style' => ['green', null, ['bold']],
+            'logging' => true,
+            'log_options' => [
+                'apply_style_to_full_line' => true
+            ]
+        ],
         'horizon' => [
             'command' => ['php', 'artisan', 'horizon'],
             'style' => ['cyan', null, ['bold']],
